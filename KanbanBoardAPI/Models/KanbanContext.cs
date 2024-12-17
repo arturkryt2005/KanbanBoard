9bf8095877dcdfe886a0aed44384a9da4e5a6155
Артур <125348508+arturkryt2005@users.noreply.github.com>
2024-12-17 18:30:08 +0300

    Connected Database

---
+++ KanbanBoardAPI/Models/KanbanContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KanbanBoardAPI.Models
+{
+    public class KanbanContext : DbContext
+    {
+        public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Column> Columns { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
