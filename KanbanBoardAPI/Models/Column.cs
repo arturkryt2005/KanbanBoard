a40e1e715ad261ce6dd1f5d4596c7f0458120a12
Артур <125348508+arturkryt2005@users.noreply.github.com>
2024-12-17 17:19:09 +0300

    Добавьте файлы проекта.

---
+++ KanbanBoardAPI/Models/Column.cs
@@ -0,0 +1,13 @@
+﻿namespace KanbanBoardAPI.Models
+{
+    public class Column
+    {
+        public int Id { get; set; }
+
+        public string ?Name { get; set; }
+
+        public int Order { get; set; }
+
+        public List<Task> ?Tasks { get; set; }
+    }
+}
---
+++ KanbanBoardAPI/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Common;
+
+namespace KanbanBoardAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string ?Title { get; set; }
+
+        public string ?Description { get; set; }
+
+        public int ColumnId { get; set; }
+
+        public Column ?Column { get; set; }
+
+        public string ?Assignee { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
